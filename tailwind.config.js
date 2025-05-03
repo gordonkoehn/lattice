ba78e6b1a463a13a3e8104b2d99260d0356d4a6d
7jep7 <je77petersen@gmail.com>
2025-05-03 18:41:04 +0100

    Cleaned up structure, tailwindCSS, remix, and postCSS somehow work together but not properly yet. Needs more work.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./app/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {},
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        icyWhite: "#F0F4F8",
+        orangePrimary: "#FF6A00",
+        orangeSecondary: "#E65C00",
+        grayMetallic: "#A2A8B4",
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+}
+
